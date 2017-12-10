9ddef4d15c302dd2c53f89d4eff75bb7abdd4c5c
Tomek <java-tomek@users.noreply.github.com>
2017-12-10 10:14:41 +0100

    add hibernate annotations

--- src/main/java/com/tomek/sdachat/model/Tweet.java
+++ src/main/java/com/tomek/sdachat/model/Tweet.java
@@ -3,10 +3,23 @@ package com.tomek.sdachat.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
 @AllArgsConstructor
+@Entity
+@Table(name = "tweets")
 public class Tweet {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column
     private int id;
+    @Column(name = "user_id")
     private int userId;
     private long timestamp;
     private String message;
