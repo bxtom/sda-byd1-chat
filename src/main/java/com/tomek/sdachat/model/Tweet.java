809dd97f1502a9e6878df7961895ef3f34574d73
Tomek <java-tomek@users.noreply.github.com>
2017-12-10 11:50:20 +0100

    fix hibernate relation

--- src/main/java/com/tomek/sdachat/model/Tweet.java
+++ src/main/java/com/tomek/sdachat/model/Tweet.java
@@ -2,30 +2,35 @@ package com.tomek.sdachat.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "tweets")
 public class Tweet {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private int id;
-    @Column(name = "user_id")
-    private int userId;
     private long timestamp;
     private String message;
-    @ManyToOne
-    @JoinColumn(name="user_id", nullable=false)
+    @ManyToOne(cascade = CascadeType.ALL)
     private User user;
+
+    public Tweet(long timestamp, String message, User user) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.user = user;
+    }
 }
