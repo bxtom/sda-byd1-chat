7b86ccd0e176c888966dc84d15d169915cc5b408
Tomek <java-tomek@users.noreply.github.com>
2017-12-10 12:38:12 +0100

    add login servlet with cookie

---
+++ src/main/java/com/tomek/sdachat/servlets/LoginServlet.java
@@ -0,0 +1,28 @@
+package com.tomek.sdachat.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "loginServlet", value = "/loginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nick = req.getParameter("nick");
+        String password = req.getParameter("password");
+
+        if (nick.equals("tomek") && password.equals("123")) {
+            Cookie authorCookie = new Cookie("nick", nick);
+            authorCookie.setMaxAge(60);
+            resp.addCookie(authorCookie);
+
+            resp.sendRedirect("/index.jsp");
+        } else {
+            resp.sendRedirect("/login.jsp?error=1");
+        }
+    }
+}
