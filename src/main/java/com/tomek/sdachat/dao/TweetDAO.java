21b0391697969fefd1ab342176cc45dd7132d005
Tomek <java-tomek@users.noreply.github.com>
2017-12-13 13:03:07 +0100

    change dao classes to extend generic dao

--- src/main/java/com/tomek/sdachat/dao/TweetDAO.java
+++ src/main/java/com/tomek/sdachat/dao/TweetDAO.java
@@ -1,95 +1,9 @@
 package com.tomek.sdachat.dao;
 
 import com.tomek.sdachat.model.Tweet;
-import com.tomek.sdachat.utility.HibernateUtility;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
 
-import java.util.List;
-
-public class TweetDAO {
-    public void addTweet(Tweet tweet) {
-        Session session = HibernateUtility.getHibernateSession();
-
-        try {
-            session.beginTransaction();
-            session.save(tweet);
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            if (session.getTransaction() != null) {
-                session.getTransaction().rollback();
-            }
-
-            e.printStackTrace();
-        }
-    }
-
-    public Tweet getTweet(int id) {
-        Session session = HibernateUtility.getHibernateSession();
-        Tweet result = null;
-
-        try {
-            session.beginTransaction();
-            result = session.get(Tweet.class, id);
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            if (session.getTransaction() != null) {
-                session.getTransaction().rollback();
-            }
-
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-    public void updateTweet(Tweet tweet) {
-        Session session = HibernateUtility.getHibernateSession();
-
-        try {
-            session.beginTransaction();
-            session.update(tweet);
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            if (session.getTransaction() != null) {
-                session.getTransaction().rollback();
-            }
-
-            e.printStackTrace();
-        }
-    }
-
-    public void deleteTweet(Tweet tweet) {
-        Session session = HibernateUtility.getHibernateSession();
-
-        try {
-            session.beginTransaction();
-            session.remove(tweet);
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            if (session.getTransaction() != null) {
-                session.getTransaction().rollback();
-            }
-
-            e.printStackTrace();
-        }
-    }
-
-    public List<Tweet> getAllTweets() {
-        Session session = HibernateUtility.getHibernateSession();
-        List tweets = null;
-
-        try {
-            session.beginTransaction();
-            tweets = session.createQuery("FROM Tweet").list();
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            if (session.getTransaction() != null) {
-                session.getTransaction().rollback();
-            }
-
-            e.printStackTrace();
-        }
-
-        return tweets;
+public class TweetDAO extends GenericDAO<Tweet> {
+    public TweetDAO() {
+        type = Tweet.class;
     }
 }
--- src/main/java/com/tomek/sdachat/dao/UserDAO.java
+++ src/main/java/com/tomek/sdachat/dao/UserDAO.java
@@ -1,26 +1,9 @@
 package com.tomek.sdachat.dao;
 
 import com.tomek.sdachat.model.User;
-import com.tomek.sdachat.utility.HibernateUtility;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
 
-public class UserDAO {
-    private Session session = HibernateUtility.getHibernateSession();
-
-    public User getUser(int id) {
-        User result = null;
-
-        try {
-            session.beginTransaction();
-            result = session.get(User.class, id);
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            if (session.getTransaction() != null) {
-                session.getTransaction().rollback();
-            }
-            e.printStackTrace();
-        }
-        return result;
+public class UserDAO extends GenericDAO<User> {
+    public UserDAO() {
+        type = User.class;
     }
 }
