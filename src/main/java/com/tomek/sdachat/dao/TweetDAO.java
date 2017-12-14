d97ea7a4808309d8a8a46f1c267596b91271c508
Tomek <java-tomek@users.noreply.github.com>
2017-12-14 09:12:37 +0100

    add method to get all tweets in newest-first order

--- src/main/java/com/tomek/sdachat/dao/TweetDAO.java
+++ src/main/java/com/tomek/sdachat/dao/TweetDAO.java
@@ -1,9 +1,33 @@
 package com.tomek.sdachat.dao;
 
 import com.tomek.sdachat.model.Tweet;
+import com.tomek.sdachat.utility.HibernateUtility;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import java.util.List;
 
 public class TweetDAO extends GenericDAO<Tweet> {
     public TweetDAO() {
         type = Tweet.class;
+    }
+
+    public List<Tweet> getAllTweetsNewestFirst() {
+        Session session = HibernateUtility.getHibernateSession();
+        List tweets = null;
+
+        try {
+            session.beginTransaction();
+            tweets = session.createQuery("from Tweet order by timestamp desc").list();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            if (session.getTransaction() != null) {
+                session.getTransaction().rollback();
+            }
+
+            e.printStackTrace();
+        }
+
+        return tweets;
     }
 }
