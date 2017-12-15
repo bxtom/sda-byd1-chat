1365d3942b1c2bb202bd1f45eafc1cb2d95f89c4
Tomek <java-tomek@users.noreply.github.com>
2017-12-15 10:49:52 +0100

    add method to verify if nick is taken add method to find one user with specific nick

--- src/main/java/com/tomek/sdachat/dao/UserDAO.java
+++ src/main/java/com/tomek/sdachat/dao/UserDAO.java
@@ -38,4 +38,56 @@ public class UserDAO extends GenericDAO<User> {
         }
         return user;
     }
+
+    public User findOneUserByNick(String nick) {
+        Session session = HibernateUtility.getHibernateSession();
+        User user = null;
+
+        try {
+            session.beginTransaction();
+
+            String hql = "from User u where upper(u.nick) = upper(:nick)";
+            List users = session.createQuery(hql)
+                    .setParameter("nick", nick)
+                    .list();
+
+            if (users.size() > 0)
+                user = (User) users.get(0);
+
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            if (session.getTransaction() != null) {
+                session.getTransaction().rollback();
+            }
+
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    public Boolean isNickTaken(String nick) {
+        Session session = HibernateUtility.getHibernateSession();
+        Boolean result = false;
+
+        try {
+            session.beginTransaction();
+
+            String hql = "from User u where upper(u.nick) = upper(:nick)";
+            List users = session.createQuery(hql)
+                    .setParameter("nick", nick)
+                    .list();
+
+            if (users.size() > 0)
+                result = true;
+
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            if (session.getTransaction() != null) {
+                session.getTransaction().rollback();
+            }
+
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
